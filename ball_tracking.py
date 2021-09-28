2b70c423909ac187bf8e2f005f2cf773dda69c0c
Razg93 <50642442+Razg93@users.noreply.github.com>
2021-09-28 14:34:01 +0300

    Update ball_tracking.py

--- ball_tracking.py
+++ ball_tracking.py
@@ -1,6 +1,3 @@
-# USAGE
-# python ball_tracking.py --video ball_tracking_example.mp4
-# python ball_tracking.py
 
 # import the necessary packages
 from collections import deque
@@ -122,11 +119,6 @@ while True:
         # them
         if pts[i - 1] is None or pts[i] is None:
             continue
-
-        # otherwise, compute the thickness of the line and
-        # draw the connecting lines
-        #thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
-        #cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
 
     # show the frame to our screen
     cv2.imshow("Frame", frame)
